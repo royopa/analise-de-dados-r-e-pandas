5a07d94da0caf63a1d3a43623272908959216b09
Rodrigo Prado de Jesus <royopa@gmail.com>
2019-07-21 11:52:57 -0300

    alterado aula 2

---
+++ README.md
@@ -0,0 +1,3 @@
+O notebook do do jupyter está no caminho abaixo:
+
+https://colab.research.google.com/drive/1gfyMk_DbkQvcxtQPfnPeLoLdDhX8teCL#scrollTo=X0INm1rGBDv5
--- aula1.r
+++ aula1.r
@@ -3,12 +3,12 @@ aulas <- read_excel("/home/rodrigo/projects/analise-dados-introducao-r/datasets/
 View(aulas)
 
 # para reconhecer as variáveis do arquivo excel attach()
-# attach(aulas).
+# copia o excel para a memória.
 attach(aulas)
 
 section_id
 
-# altera o padrão para exibição
+# altera o padrão par-a exibição
 options(max.print = 40000)
 
 # head para listar os primeiros registros da lista
@@ -17,3 +17,17 @@ head(section_id)
 # ordená-los do menor para o maior, utilizaremos a função sort no Script:
 sort(section_id)
 
+# substituir o valor section_id de 999999 para 3255
+# dado inserido incorretamente na linha 33137 e coluna 3
+aulas[33137, 3] <- 3255
+
+sort(aulas$section_id)
+
+# localiza o valor alterado, para confirmar se foi mesmo alterado
+aulas[33137, 3]
+
+# a quantidade de cursos únicos, função unique(), que fornecerá uma exibição por curso.
+unique(aulas$section_id)
+
+# length conta a quantidade de registros
+length(unique(aulas$section_id))
